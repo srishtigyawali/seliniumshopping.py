ca845b95aa9f7879d6dcc16a29f2943bf5e37190
Srishti Gyawali <67185868+srishtigyawali@users.noreply.github.com>
2020-08-24 20:47:58 +0545

    Add files via upload

---
+++ web.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+driver = webdriver.Chrome('D:/python/chromedriver.exe')
+driver.get("https://www.getprice.com.au/")
+
+products = driver.find_elements_by_xpath('//div[@class="desc"]')
+
+number1 = len(products)
+
+with open("shopping.txt","w") as f:
+    for i in range(number1):
+        f.write(products[i].text + "\n")
+
+driver.close()
